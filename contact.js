5af58ac4e31d7cb08527dfb42499a8e561365ca4
SwarShah <swar12894@gmail.com>
2015-04-15 22:06:08 -0400

    Get working

--- contact.js
+++ contact.js
@@ -9,9 +9,20 @@ function connect () {
 	user : 'c647456',
 	password : 'c647456'
   });
-  conn.query('USE contact');
+  conn.query('USE c647456');
   return conn;
 }
+
+app.get('/id/:id', function(req, res){
+	var sql = 'SELECT * FROM ?? WHERE ?? = ?';
+	var inserts = ['contact', 'id', req.params.id];
+	var q = mysql.format(sql, inserts);
+	var connection = connect();
+	connection.query(q, function(err, rows){
+		res.send(rows);
+		connection.end();
+	});
+});
 
 var server = app.listen(8001, function(err){
 	console.log('Listening on port 8001');
